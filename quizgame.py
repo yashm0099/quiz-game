059746c7048cff484196337f6c0ba8f8fa87b0cb
yashm0099 <91181595+yashm0099@users.noreply.github.com>
2022-02-19 17:22:09 +0530

    Add files via upload

---
+++ quizgame.py
@@ -0,0 +1,61 @@
+print("what is capital of japan?")
+print("A - tokyo")
+print("B - Yokohoma")
+print("C - osska")
+print("D - Nagoya")
+
+option1 = "A"
+option2 = "B"
+option3 = "C"
+option4 = "D"
+q = input("Please choose your answer")
+if q == str(option1):
+    print("Right")
+elif q == str(option2):
+    print("Wrong")
+elif q == str(option3):
+    print("Wrong")
+elif q == str(option4):
+    print("Wrong")
+print("when kohli media was founded?")
+print("A - 2001")
+print("B - 2006")
+print("c - 2003")
+print("D - 2022")
+option1 = "A"
+option2 = "B"
+option3 = "C"
+option4 = "D"
+q = input("please choose your answer")
+if q == str(option1):
+        print("worng")
+elif q == str(option2):
+        print("right")
+elif q == str(option3):
+        print("Wrong")
+elif q == str(option4):
+        print("Wrong")
+print("who is ceo of google?")
+print("A - satya nadela")
+print("B - Elon musk")
+print("c - sundar pichai")
+print("D - steve jobs")
+option1 = "A"
+option2 = "B"
+option3 = "C"
+option4 = "D"
+q = input("please choose your answer")
+if q == str(option1):
+        print("worng")
+elif q == str(option2):
+        print("wrong")
+elif q == str(option3):
+        print("Right")
+elif q == str(option4):
+        print("Wrong")
+
+
+
+
+
+
